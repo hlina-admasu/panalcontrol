3aad523957ac63008d9fa8bf36062ef213dece33
hlina-admasu <hilinaadmasu@gmail.com>
2025-04-29 12:20:50 -0700

    Add files via upload

---
+++ prjrans/includes/config.php
@@ -0,0 +1,11 @@
+<?php
+// Steps 1: Configuration file for our MySql database
+
+
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'qwerty');
+define('DB_NAME', 'prjrans');
+?>
+
---
+++ prjrans/public/logout.php
@@ -0,0 +1,8 @@
+<?php
+// Step 1 : Logout Sessions
+session_start();
+$_SESSION = array();
+session_destroy();
+header("location: login.php");
+exit;
+?>
